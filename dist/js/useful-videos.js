b48e4d5a89f2b4c936b40ec14b6ac2dee7042686
Maurice van Creij <maurice.vancreij@webqem.com>
2015-12-16 09:44:35 +1100

    Switch to Gulp

--- dist/js/useful-videos.js
+++ dist/js/useful-videos.js
@@ -422,3 +422,60 @@ var useful = useful || {};
   }
 
 })();
+
+/*
+	Source:
+	van Creij, Maurice (2014). "useful.videos.js: Embedded video that doesn't cause traffic until clicked.", version 20141127, http://www.woollymittens.nl/.
+
+	License:
+	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+*/
+
+// create the constructor if needed
+var useful = useful || {};
+
+// extend the constructor
+useful.Videos = function () {
+
+	// PROPERTIES
+
+	"use strict";
+
+	this.config = {
+		'template' : '<iframe src="//www.youtube.com/embed/{id}?autoplay=1" height="400" width="300"></iframe>',
+		'separator' : 'v='
+	};
+
+	// METHODS
+
+	this.init = function (config) {
+		// store the config
+		for (var property in config) { this.config[property] = config[property]; }
+		// add the click events
+		var previews = this.config.elements;
+		for (var a = 0, b = previews.length; a < b; a += 1) {
+			// add click handlers
+			previews[a].addEventListener('click', this.onPreviewClicked.bind(this, previews[a]));
+		}
+		// return the object
+		return this;
+	};
+
+	// EVENTS
+
+	this.onPreviewClicked = function (element) {
+		// cancel the click
+		event.preventDefault();
+		// replace the element with the iframe
+		var id = element.getAttribute('href').split(this.config.separator);
+		var wrapper = document.createElement('span');
+		wrapper.innerHTML = this.config.template.replace('{id}', id[id.length - 1]);
+		element.parentNode.replaceChild(wrapper, element);
+	};
+
+};
+
+// return as a require.js module
+if (typeof module !== 'undefined') {
+	exports = module.exports = useful.Videos;
+}
--- src/js/useful-videos.js
+++ src/js/useful-videos.js
@@ -8,29 +8,45 @@
 
 // create the constructor if needed
 var useful = useful || {};
+useful.Videos = useful.Videos || function () {};
 
-// extend the constructor
-useful.Videos = function () {
-
-	// PROPERTIES
-
+// extend the prototype with the init function
+useful.Videos.prototype.init = function (config) {
+	// turn on strict mode
 	"use strict";
-
+	// default config
 	this.config = {
 		'template' : '<iframe src="//www.youtube.com/embed/{id}?autoplay=1" height="400" width="300"></iframe>',
 		'separator' : 'v='
 	};
+	// store the config
+	for (var name in config) { this.config[name] = config[name]; }
+	// bind the components
+	this.previews = new this.Previews().init(this);
+	// return the object
+	return this;
+};
+
+// extend the prototype with the scroll monitoring component
+useful.Videos.prototype.Previews = function () {
+
+	// PROPERTIES
+
+	"use strict";
+	this.context = null;
+	this.config = {};
 
 	// METHODS
 
-	this.init = function (config) {
-		// store the config
-		for (var property in config) { this.config[property] = config[property]; }
-		// add the click events
-		var previews = this.config.elements;
-		for (var a = 0, b = previews.length; a < b; a += 1) {
+	this.init = function (context) {
+		// store the context
+		this.context = context;
+		this.config = context.config;
+		// set the event handlers
+		var elements = this.config.elements;
+		for (var a = 0, b = elements.length; a < b; a += 1) {
 			// add click handlers
-			previews[a].addEventListener('click', this.onPreviewClicked(previews[a]));
+			elements[a].addEventListener('click', this.onPreviewClicked.bind(this, elements[a]));
 		}
 		// return the object
 		return this;
@@ -39,16 +55,13 @@ useful.Videos = function () {
 	// EVENTS
 
 	this.onPreviewClicked = function (element) {
-		var _this = this;
-		return function (evt) {
-			// cancel the click
-			evt.preventDefault();
-			// replace the element with the iframe
-			var id = element.getAttribute('href').split(_this.config.separator);
-			var wrapper = document.createElement('span');
-			wrapper.innerHTML = _this.config.template.replace('{id}', id[id.length - 1]);
-			element.parentNode.replaceChild(wrapper, element);
-		};
+		// cancel the click
+		event.preventDefault();
+		// replace the element with the iframe
+		var id = element.getAttribute('href').split(this.config.separator);
+		var wrapper = document.createElement('span');
+		wrapper.innerHTML = this.config.template.replace('{id}', id[id.length - 1]);
+		element.parentNode.replaceChild(wrapper, element);
 	};
 
 };
