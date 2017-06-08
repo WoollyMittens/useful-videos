bab0c3a72585c8b0fdeb993a97d5e6c846463f60
Maurice van Creij <maurice.vancreij@webqem.com>
2017-06-09 09:00:19 +1000

    Configure dependencies in build script

--- dist/js/useful-videos.js
+++ dist/js/useful-videos.js
@@ -433,29 +433,45 @@ var useful = useful || {};
 
 // create the constructor if needed
 var useful = useful || {};
+useful.Videos = useful.Videos || function () {};
 
-// extend the constructor
-useful.Videos = function () {
-
-	// PROPERTIES
-
+// extend the prototype with the init function
+useful.Videos.prototype.init = function (config) {
+	// turn on strict mode
 	"use strict";
-
+	// default config
 	this.config = {
 		'template' : '<iframe src="//www.youtube.com/embed/{id}?autoplay=1" height="400" width="300"></iframe>',
 		'separator' : 'v='
 	};
+	// store the config
+	for (var name in config) { this.config[name] = config[name]; }
+	// bind the components
+	this.previews = new this.Previews().init(this);
+	// return the object
+	return this;
+};
+
+// extend the prototype with the scroll monitoring component
+useful.Videos.prototype.Previews = function () {
+
+	// PROPERTIES
+
+	"use strict";
+	this.context = null;
+	this.config = {};
 
 	// METHODS
 
-	this.init = function (config) {
-		// store the config
-		for (var property in config) { this.config[property] = config[property]; }
-		// add the click events
-		var previews = this.config.elements;
-		for (var a = 0, b = previews.length; a < b; a += 1) {
+	this.init = function (context) {
+		// store the context
+		this.context = context;
+		this.config = context.config;
+		// set the event handlers
+		var elements = this.config.elements;
+		for (var a = 0, b = elements.length; a < b; a += 1) {
 			// add click handlers
-			previews[a].addEventListener('click', this.onPreviewClicked.bind(this, previews[a]));
+			elements[a].addEventListener('click', this.onPreviewClicked.bind(this, elements[a]));
 		}
 		// return the object
 		return this;
